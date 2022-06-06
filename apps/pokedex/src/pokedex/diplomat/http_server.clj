b3797e25ab73d071ac46d99f13e7d9bd7dd446b1
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-05 21:00:35 -0300

    update

--- apps/pokedex/src/pokedex/diplomat/http_server.clj
+++ apps/pokedex/src/pokedex/diplomat/http_server.clj
@@ -1,6 +1,5 @@
 (ns pokedex.diplomat.http_server
-  :require [io.pedestal.http.route :refer [expand-routes]
-           [ring.util.response :as ring-resp]])
+  :require [ring.util.response :as ring-resp])
 
 (defn current-version
       [request]
